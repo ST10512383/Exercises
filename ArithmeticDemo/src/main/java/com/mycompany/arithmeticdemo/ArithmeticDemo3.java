dfab16c6307531c7dc3365f2d3951bea151e1c46
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 14:49:45 +0200

    Copy from ArithmeticDemo2

--- ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo3.java
+++ ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo3.java
@@ -1,8 +1,31 @@
 package com.mycompany.arithmeticdemo;
 
+import java.util.Scanner;
+
 public class ArithmeticDemo3 {
 
     public static void main(String[] args) {
+                    
+        Scanner input = new Scanner(System.in);
+        
+        double firstNumber;
+        double secondNumber;
+        double sum;
+        double difference;
+        double average;
+        
+        System.out.println("Please enter a double -> ");
+        firstNumber = input.nextDouble();
+        System.out.println("Please enter another double -> ");
+        secondNumber = input.nextDouble();
+        
+        sum = firstNumber + secondNumber;
+        difference = firstNumber + secondNumber;
+        average = sum / 2;
+        
+        System.out.println("The sum of " + firstNumber + " + " + secondNumber + " is " + sum);
+        System.out.println("The difference between " + firstNumber + " - " + secondNumber + " is " + difference);
+        System.out.println("The average of " + firstNumber + " and " + secondNumber + " is " + average);
     }
     
 }
