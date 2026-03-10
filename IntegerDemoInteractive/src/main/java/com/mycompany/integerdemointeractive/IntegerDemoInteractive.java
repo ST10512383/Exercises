8b5bdfb3d8563d1dcf857db2ff6a1d10d6feae74
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 11:25:00 +0200

    Add program functionality

--- IntegerDemoInteractive/src/main/java/com/mycompany/integerdemointeractive/IntegerDemoInteractive.java
+++ IntegerDemoInteractive/src/main/java/com/mycompany/integerdemointeractive/IntegerDemoInteractive.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 public class IntegerDemoInteractive {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         
+        int anInt;
+        byte aByte;
+        short aShort;
+        long aLong;
+        
+        System.out.print("Please enter an Integer > ");
+        anInt = userInput.nextInt();
+        
+        System.out.print("Please enter a Byte integer > ");
+        aByte = userInput.nextByte();
+        
+        System.out.print("Please enter a Short integer > ");
+        aLong = userInput.nextLong();
+        
+        userInput.close();
     }
 }
