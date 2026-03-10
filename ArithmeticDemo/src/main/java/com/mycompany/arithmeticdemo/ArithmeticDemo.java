31530c46a98f1365accf757c7a1a88fac15e910f
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 14:03:35 +0200

    Add scanner import

--- ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo.java
+++ ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class ArithmeticDemo {
 
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
         int firstNumber;
         int secondNumber;
         int sum;
