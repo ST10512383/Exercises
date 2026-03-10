0694c24c3e1c49246628f3696f3c23ba4cb9fd36
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 11:19:54 +0200

    Created project files

---
+++ IntegerDemoInteractive/src/main/java/com/mycompany/integerdemointeractive/IntegerDemoInteractive.java
@@ -0,0 +1,10 @@
+package com.mycompany.integerdemointeractive;
+
+import java.util.Scanner;
+
+public class IntegerDemoInteractive {
+
+    public static void main(String[] args) {
+        
+    }
+}
