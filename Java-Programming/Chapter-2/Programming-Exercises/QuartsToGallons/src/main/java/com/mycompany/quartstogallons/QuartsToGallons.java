23788c8eb2fae70ef992492ed5997bb3b494cdc7
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-11 18:13:00 +0200

    Update QuartsToGallons.java

--- Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
+++ Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class QuartsToGallons {
 
     public static void main(String[] args) {
-        
+        final double QUARTS = 4;
         Scanner input = new Scanner(System.in);
         
-        final double QUARTS = 4;
-        double quartsNeeded = 18;
+        System.out.println("What are the quarts needed? ");
+        
+        double quartsNeeded = input.nextDouble();
         double leftOverQuarts = ((quartsNeeded / QUARTS) - 4) * 4;
         
         System.out.println("A job that needs " + (int) quartsNeeded + " quarts requires " + (int) QUARTS + " gallons plus " + (int) leftOverQuarts + " quarts");
