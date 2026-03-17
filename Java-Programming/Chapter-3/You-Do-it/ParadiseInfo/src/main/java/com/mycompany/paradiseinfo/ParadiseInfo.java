b17802e58435a56577865b4cff32b7b9d5aa6297
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-17 11:42:44 +0200

    Invoke method, Add print statements

--- Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/ParadiseInfo.java
+++ Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/ParadiseInfo.java
@@ -3,9 +3,13 @@ package com.mycompany.paradiseinfo;
 public class ParadiseInfo {
 
     public static void main(String[] args) {
+        // Invoke displayInfo
+        displayInfo();
     }
     
+    // Create displayInfo that prints out statements to output
     public static void displayInfo(){
-        
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
     }
 }
