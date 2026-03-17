7921bf8da919f4224d06d69dfaee011b5ca3cf3e
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-17 12:01:57 +0200

    Create TestInfo, Add displayInfo
    This is to test how methods can be used across classes without being in the same file.

---
+++ Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/TestInfo.java
@@ -0,0 +1,10 @@
+package com.mycompany.paradiseinfo;
+
+public class TestInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Calling a method from another class:");
+        ParadiseInfo.displayInfo();
+    }
+    
+}
